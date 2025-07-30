607ccac6531aecb4e1c08f71c201d0ea5ed19965
Pro100x3mal <pro100x3mal@gmail.com>
2025-07-30 23:17:32 +0300

    feat: integrate MessageStore interface with GoMock implementation, refactor webhook for modularity, and enhance tests for message handling and dynamic responses

--- cmd/skill/main.go
+++ cmd/skill/main.go
@@ -1,14 +1,10 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
 	"net/http"
 	"strings"
-	"time"
 
 	"github.com/Pro100x3mal/alice-skill/internal/logger"
-	"github.com/Pro100x3mal/alice-skill/internal/models"
 	"go.uber.org/zap"
 )
 
@@ -63,65 +59,9 @@ func run() error {
 		return err
 	}
 
+	appInstance := newApp(nil)
+
 	logger.Log.Info("Running server", zap.String("address", flagRunAddr))
-	// оборачиваем хендлер webhook в middleware с логированием
-	return http.ListenAndServe(flagRunAddr, logger.RequestLogger(webhook))
-}
-
-func webhook(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		logger.Log.Debug("got request with bad method", zap.String("method", r.Method))
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		return
-	}
-
-	logger.Log.Debug("decoding request")
-	var req models.Request
-	dec := json.NewDecoder(r.Body)
-	if err := dec.Decode(&req); err != nil {
-		logger.Log.Debug("cannot decode request JSON body", zap.Error(err))
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	if req.Request.Type != models.TypeSimpleUtterance {
-		logger.Log.Debug("unsupported request type", zap.String("type", req.Request.Type))
-		w.WriteHeader(http.StatusUnprocessableEntity)
-		return
-	}
-
-	text := "Для вас нет новых сообщений."
-
-	// первый запрос новой сессии
-	if req.Session.New {
-		// обрабатываем поле Timezone запроса
-		tz, err := time.LoadLocation(req.Timezone)
-		if err != nil {
-			logger.Log.Debug("cannot parse timezone")
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-
-		// получаем текущее время в часовом поясе пользователя
-		now := time.Now().In(tz)
-		hour, minute, _ := now.Clock()
-
-		// формируем текст ответа
-		text = fmt.Sprintf("Точное время %d часов, %d минут. %s", hour, minute, text)
-	}
-
-	resp := models.Response{
-		Response: models.ResponsePayload{
-			Text: text,
-		},
-		Version: "1.0",
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	enc := json.NewEncoder(w)
-	if err := enc.Encode(resp); err != nil {
-		logger.Log.Debug("error encoding response", zap.Error(err))
-		return
-	}
-	logger.Log.Debug("sending HTTP 200 response")
+	// обернём хендлер webhook в middleware с логированием и поддержкой gzip
+	return http.ListenAndServe(flagRunAddr, logger.RequestLogger(gzipMiddleware(appInstance.webhook)))
 }
